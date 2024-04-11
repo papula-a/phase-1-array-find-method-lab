ca6aba2cf7e5e6071e5986787add4d094803488a
papula-a <pa20381@auis.edu.krd>
2024-04-11 16:15:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    
+    if (win) {
+        return win.year;
+    } else {
+        return undefined;
+    }
+}
+a
+const record = [
+    { year: 1997, result: "W" },
+    { year: 1998, result: "L" },
+    { year: 1999, result: "W" }
+];
+
+
